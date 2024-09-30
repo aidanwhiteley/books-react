13d288c70f2418afc2d222a8f067c74e0082e608
Aidan Whiteley <aidan.whiteley@gmail.com>
2024-09-30 18:49:43 +0100

    WiP

--- src/components/BookDetails/BookDetails.tsx
+++ src/components/BookDetails/BookDetails.tsx
@@ -2,6 +2,7 @@ import { BookProps } from "../../routes/BookDetailsRoute";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import "./BossDetails.css";
+import purify from "dompurify";
 
 export default function BookDetails(props: BookProps) {
 
@@ -9,6 +10,8 @@ export default function BookDetails(props: BookProps) {
     const bookCover = book.googleBookDetails.volumeInfo.imageLinks.thumbnail ? book.googleBookDetails.volumeInfo.imageLinks.thumbnail
             : book.googleBookDetails.volumeInfo.imageLinks.smallThumbnail;
     const altText = 'Picture of book cover for ' + book.title;
+    const displayGooglePreview = book.googleBookDetails && book.googleBookDetails.accessInfo.embeddable &&
+        book.googleBookDetails.accessInfo.viewability !== 'NO_PAGES';
 
     return (
 
@@ -28,12 +31,27 @@ export default function BookDetails(props: BookProps) {
                                 <p className="rating"><b>Rating: </b>{book.rating.toLocaleLowerCase()}</p>
                                 <p><b>Review Date: </b>{book.createdDateTime[2] + '-' + book.createdDateTime[1] + '-' + book.createdDateTime[0]}</p>
                                 <p><b>Genre: </b>{book.genre}</p>
-                                <p className="reviewDetail"><b>Reviewer's Summary:</b><span className="reviewDetailSummary">{book.summary}</span></p>
+                                <p className="reviewDetail"><b>Reviewer's Summary:</b><span className="reviewDetailSummary"
+                                    dangerouslySetInnerHTML={{__html: purify.sanitize(book.summary)}}></span></p>
+                                {displayGooglePreview &&
+                                    <p>
+                                    <b className="me-3">Book Preview:</b>
+                                        <a href={"https://books.google.co.uk/books?id=" + book.googleBookId + "&printsec=frontcover#v=onepage&q&f=true"} target="_blank">
+                                            <img className="" src="/images/gbs_preview_button1.gif" />
+                                        </a>
+                                    </p>
+                                }
                             </div>
                         </Tab>
 
                         <Tab eventKey="googleBookSummary" title="Google Book Summary">
-                            Google book summary
+                            <div className="bookInfo">
+                                {bookCover && 
+                                    <img src={bookCover.replace('http://', 'https://')} className="float-start rounded img-thumbnail me-3" alt={altText} />
+                                }
+                                <p><b>Google Summary: </b></p>
+                                <div id="googleSummaryDetail" dangerouslySetInnerHTML={{__html: purify.sanitize(book.googleBookDetails.volumeInfo.description)}}></div>
+                            </div>
                         </Tab>
 
                     </Tabs>
