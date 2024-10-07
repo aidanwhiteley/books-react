3ae8e73bbad563081eea807b0af4780fd887684f
Aidan Whiteley <aidan.whiteley@gmail.com>
2024-10-07 22:00:12 +0100

    Working on type aheads

--- src/components/BooksFind/BooksFind.tsx
+++ src/components/BooksFind/BooksFind.tsx
@@ -1,12 +1,41 @@
 import { BooksFindProps } from "../../routes/BooksFindRoute";
 import BooksTable from "../BooksTable/BooksTable";
+import { Typeahead } from 'react-bootstrap-typeahead';
+import { Option } from 'react-bootstrap-typeahead/types/types';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import 'react-bootstrap-typeahead/css/Typeahead.bs5.css';
+import { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import './BooksFind.css';
+import { Author } from "../../apis/HttpDataApis";
 
-export default function BooksFind() {
+export default function BooksFind(props: BooksFindProps) {
+
+    const [author, setAuthor] = useState<Option[]>([]);
+
+    const authorsDisplay = props.authors.map(anAuthor => {
+        const text = anAuthor.countOfBooks > 1 ? ' books' : ' book';
+        return {
+            label: anAuthor.author + ' (' + anAuthor.countOfBooks + text + ')'
+        }
+    });
 
     return (
         <>
             <h2>Find a book review</h2>
 
+            <Form.Group>
+                <Form.Label>Find Reviews By Author</Form.Label>
+                <Typeahead
+                    placeholder="Choose a author..."
+                    id="authors-select"
+                    clearButton
+                    onChange={(selected) => setAuthor(selected)}
+                    options={authorsDisplay}
+                    selected={author}
+                    highlightOnlyResult
+                    />
+            </Form.Group>
             <hr/>
 
             {/* <BooksTable {...props} /> */}
--- src/routes/BooksFindRoute.tsx
+++ src/routes/BooksFindRoute.tsx
@@ -1,33 +1,37 @@
 import BooksFind from "../components/BooksFind/BooksFind";
 import { useLoaderData, LoaderFunction, redirect} from "react-router-typesafe";
-import { BooksQueryResult, getAuthors, getGenres, getReaders } from "../apis/HttpDataApis";
+import { getAuthors, getGenres, getReaders, Author, Genre, Reader } from "../apis/HttpDataApis";
 
 
 
 export const loader = (async ({}) => {
 
-  const authors = getAuthors();
-  const genres = getGenres();
-  const readers = getReaders();
+  const authors = await getAuthors();
+  const genres = await getGenres();
+  const readers = await getReaders();
 
-  return await {authors, genres, readers};
+  return {authors, genres, readers};
 
 }) satisfies LoaderFunction;
 
 export interface BooksFindProps {
-  booksQueryResult: BooksQueryResult;
+  authors: Author[];
+  genres: Genre[];
+  readers: Reader[];
 }
 
 export default function BooksFindRoute() {
 
   const data = useLoaderData<typeof loader>();
 
-  // const booksProps : BooksProps = {
-  //   booksQueryResult: {},
-  // }
+  const booksFindProps : BooksFindProps = {
+    authors: data.authors,
+    genres: data.genres,
+    readers: data.readers
+  }
 
   return (
-    <BooksFind />
+    <BooksFind {...booksFindProps} />
   )
 
 }
