c66bc090520bb0417bc89ca506a075a8ce090b6d
Aidan Whiteley <aidan.whiteley@gmail.com>
2024-10-26 22:45:01 +0100

    Added API by config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
+export default ( mode: string ) => {
 
   const env = loadEnv(mode, process.cwd());
   const API_URL = `${env.VITE_API_URL ?? 'http://localhost:8080'}`;
